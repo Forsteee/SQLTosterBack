d202bc0a0174798d4aa3d73726c0e938078456f8
Forsteee <55308823+Forsteee@users.noreply.github.com>
2021-06-04 12:58:26 +0500

    answer

--- src/testingapi/testing.module.ts
+++ src/testingapi/testing.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { testingController } from './testing.controller';
 import { testingService } from './testing.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from '../entities/tasks.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [testingController],
   providers: [testingService],
 })
